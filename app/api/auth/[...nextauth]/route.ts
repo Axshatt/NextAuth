a80c10cdfa89298ec984f4d002e5322f3f3242ac
Akshat <codepreneur.in@gmail.com>
2025-08-03 23:32:41 +0530

    added credentials next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,40 @@
+import { log } from "console";
 import NextAuth from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
 
+ 
 
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Login with email",
 
- const handler = NextAuth();
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "Akshat@gmail.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const username = credentials?.username;
+                const password = credentials?.password;
+                const user = { id: "1", name: username, email: password }
 
- export {handler as GET , handler as POST}
+                if (user) {
+
+                    console.log(user);
+                    
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            }
+        })
+    ]
+});
+
+export { handler as GET, handler as POST }
