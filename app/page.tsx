00e68e3a29fe7624bcf870fe4ac5874b782390a1
Akshat <codepreneur.in@gmail.com>
2025-08-04 21:25:05 +0530

    added usesession the ugly way

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+"use client"
+import { SessionProvider, useSession } from "next-auth/react";
 
-export default function Home() {
-  return (
-    <div>
-      Hello
+export default function Home(){
+  return <SessionProvider>
+    <RealHome/>
+  </SessionProvider>
+}
+
+
+
+ function RealHome(){
+
+   const session = useSession();
+   return (
+     <div>
+      {session.status ==="authenticated"? "logout":"Sign in"}
+      
+      
     </div>
   );
 }
+
